007d4ad2c8715e1b1e54274585c32f7603f608fb
Wendell Lee <2951404774@stu.xupt.edu.cn>
2026-06-24 19:08:32 +0800

    .xml配置

---
+++ Debug/ra/fsp/src/bsp/mcu/all/bsp_common.d
@@ -0,0 +1,96 @@
+ra/fsp/src/bsp/mcu/all/bsp_common.o: \
+ ../ra/fsp/src/bsp/mcu/all/bsp_common.c \
+ E:/Renesas/prj/RA8P1_workspace/Embedded_Competition_Renesas_Game/ra/fsp/inc/api/bsp_api.h \
+ E:/Renesas/prj/RA8P1_workspace/Embedded_Competition_Renesas_Game/ra/fsp/inc/api/fsp_common_api.h \
+ E:/Renesas/prj/RA8P1_workspace/Embedded_Competition_Renesas_Game/ra/fsp/inc/fsp_version.h \
+ E:/Renesas/prj/RA8P1_workspace/Embedded_Competition_Renesas_Game/ra_cfg/fsp_cfg/bsp/bsp_cfg.h \
+ E:/Renesas/prj/RA8P1_workspace/Embedded_Competition_Renesas_Game/ra_gen/bsp_clock_cfg.h \
+ E:/Renesas/prj/RA8P1_workspace/Embedded_Competition_Renesas_Game/ra_cfg/fsp_cfg/bsp/bsp_mcu_family_cfg.h \
+ E:/Renesas/prj/RA8P1_workspace/Embedded_Competition_Renesas_Game/ra_cfg/fsp_cfg/bsp/bsp_mcu_device_pn_cfg.h \
+ E:/Renesas/prj/RA8P1_workspace/Embedded_Competition_Renesas_Game/ra_cfg/fsp_cfg/bsp/bsp_mcu_device_cfg.h \
+ E:/Renesas/prj/RA8P1_workspace/Embedded_Competition_Renesas_Game/ra/fsp/src/bsp/mcu/ra6m5/bsp_mcu_info.h \
+ E:/Renesas/prj/RA8P1_workspace/Embedded_Competition_Renesas_Game/ra/fsp/src/bsp/mcu/ra6m5/bsp_elc.h \
+ E:/Renesas/prj/RA8P1_workspace/Embedded_Competition_Renesas_Game/ra/fsp/src/bsp/mcu/ra6m5/bsp_feature.h \
+ E:/Renesas/prj/RA8P1_workspace/Embedded_Competition_Renesas_Game/ra/fsp/src/bsp/mcu/ra6m5/bsp_peripheral.h \
+ E:/Renesas/prj/RA8P1_workspace/Embedded_Competition_Renesas_Game/ra_cfg/fsp_cfg/bsp/bsp_mcu_ofs_cfg.h \
+ E:/Renesas/prj/RA8P1_workspace/Embedded_Competition_Renesas_Game/ra_cfg/fsp_cfg/bsp/board_cfg.h \
+ E:/Renesas/prj/RA8P1_workspace/Embedded_Competition_Renesas_Game/ra_gen/vector_data.h \
+ E:/Renesas/prj/RA8P1_workspace/Embedded_Competition_Renesas_Game/ra/fsp/src/bsp/mcu/all/bsp_exceptions.h \
+ E:/Renesas/prj/RA8P1_workspace/Embedded_Competition_Renesas_Game/ra/fsp/src/bsp/cmsis/Device/RENESAS/Include/renesas.h \
+ E:/Renesas/prj/RA8P1_workspace/Embedded_Competition_Renesas_Game/ra/arm/CMSIS_6/CMSIS/Core/Include/cmsis_compiler.h \
+ E:/Renesas/prj/RA8P1_workspace/Embedded_Competition_Renesas_Game/ra/arm/CMSIS_6/CMSIS/Core/Include/cmsis_gcc.h \
+ E:/Renesas/prj/RA8P1_workspace/Embedded_Competition_Renesas_Game/ra/fsp/src/bsp/cmsis/Device/RENESAS/Include/R7FA6M5BH.h \
+ E:/Renesas/prj/RA8P1_workspace/Embedded_Competition_Renesas_Game/ra/arm/CMSIS_6/CMSIS/Core/Include/core_cm33.h \
+ E:/Renesas/prj/RA8P1_workspace/Embedded_Competition_Renesas_Game/ra/arm/CMSIS_6/CMSIS/Core/Include/cmsis_version.h \
+ E:/Renesas/prj/RA8P1_workspace/Embedded_Competition_Renesas_Game/ra/arm/CMSIS_6/CMSIS/Core/Include/cmsis_compiler.h \
+ E:/Renesas/prj/RA8P1_workspace/Embedded_Competition_Renesas_Game/ra/arm/CMSIS_6/CMSIS/Core/Include/m-profile/armv8m_mpu.h \
+ E:/Renesas/prj/RA8P1_workspace/Embedded_Competition_Renesas_Game/ra/fsp/src/bsp/cmsis/Device/RENESAS/Include/system.h \
+ E:/Renesas/prj/RA8P1_workspace/Embedded_Competition_Renesas_Game/ra/fsp/src/bsp/cmsis/Device/RENESAS/Include/system.h \
+ E:/Renesas/prj/RA8P1_workspace/Embedded_Competition_Renesas_Game/ra/fsp/src/bsp/mcu/all/bsp_common.h \
+ E:/Renesas/prj/RA8P1_workspace/Embedded_Competition_Renesas_Game/ra/fsp/inc/api/fsp_common_api.h \
+ E:/Renesas/prj/RA8P1_workspace/Embedded_Competition_Renesas_Game/ra/fsp/src/bsp/mcu/all/bsp_compiler_support.h \
+ E:/Renesas/prj/RA8P1_workspace/Embedded_Competition_Renesas_Game/ra/fsp/src/bsp/mcu/all/bsp_tfu.h \
+ E:/Renesas/prj/RA8P1_workspace/Embedded_Competition_Renesas_Game/ra/fsp/src/bsp/mcu/all/bsp_sdram.h \
+ E:/Renesas/prj/RA8P1_workspace/Embedded_Competition_Renesas_Game/ra/fsp/src/bsp/mcu/all/bsp_mmf.h \
+ E:/Renesas/prj/RA8P1_workspace/Embedded_Competition_Renesas_Game/ra/fsp/src/bsp/mcu/all/bsp_ipc.h \
+ E:/Renesas/prj/RA8P1_workspace/Embedded_Competition_Renesas_Game/ra/fsp/src/bsp/mcu/all/bsp_ospi_b.h \
+ bsp_linker_info.h \
+ E:/Renesas/prj/RA8P1_workspace/Embedded_Competition_Renesas_Game/ra/fsp/src/bsp/mcu/all/bsp_register_protection.h \
+ E:/Renesas/prj/RA8P1_workspace/Embedded_Competition_Renesas_Game/ra/fsp/src/bsp/mcu/all/bsp_irq.h \
+ E:/Renesas/prj/RA8P1_workspace/Embedded_Competition_Renesas_Game/ra/fsp/src/bsp/mcu/all/bsp_io.h \
+ E:/Renesas/prj/RA8P1_workspace/Embedded_Competition_Renesas_Game/ra/fsp/src/bsp/mcu/all/bsp_group_irq.h \
+ E:/Renesas/prj/RA8P1_workspace/Embedded_Competition_Renesas_Game/ra/fsp/src/bsp/mcu/all/bsp_clocks.h \
+ E:/Renesas/prj/RA8P1_workspace/Embedded_Competition_Renesas_Game/ra/fsp/src/bsp/mcu/all/bsp_module_stop.h \
+ E:/Renesas/prj/RA8P1_workspace/Embedded_Competition_Renesas_Game/ra/fsp/src/bsp/mcu/all/bsp_security.h \
+ E:/Renesas/prj/RA8P1_workspace/Embedded_Competition_Renesas_Game/ra/fsp/inc/fsp_features.h \
+ E:/Renesas/prj/RA8P1_workspace/Embedded_Competition_Renesas_Game/ra/fsp/inc/api/fsp_common_api.h \
+ E:/Renesas/prj/RA8P1_workspace/Embedded_Competition_Renesas_Game/ra/fsp/src/bsp/mcu/all/bsp_compiler_support.h \
+ E:/Renesas/prj/RA8P1_workspace/Embedded_Competition_Renesas_Game/ra/fsp/src/bsp/mcu/all/bsp_delay.h \
+ E:/Renesas/prj/RA8P1_workspace/Embedded_Competition_Renesas_Game/ra/fsp/src/bsp/mcu/all/bsp_mcu_api.h
+E:/Renesas/prj/RA8P1_workspace/Embedded_Competition_Renesas_Game/ra/fsp/inc/api/bsp_api.h:
+E:/Renesas/prj/RA8P1_workspace/Embedded_Competition_Renesas_Game/ra/fsp/inc/api/fsp_common_api.h:
+E:/Renesas/prj/RA8P1_workspace/Embedded_Competition_Renesas_Game/ra/fsp/inc/fsp_version.h:
+E:/Renesas/prj/RA8P1_workspace/Embedded_Competition_Renesas_Game/ra_cfg/fsp_cfg/bsp/bsp_cfg.h:
+E:/Renesas/prj/RA8P1_workspace/Embedded_Competition_Renesas_Game/ra_gen/bsp_clock_cfg.h:
+E:/Renesas/prj/RA8P1_workspace/Embedded_Competition_Renesas_Game/ra_cfg/fsp_cfg/bsp/bsp_mcu_family_cfg.h:
+E:/Renesas/prj/RA8P1_workspace/Embedded_Competition_Renesas_Game/ra_cfg/fsp_cfg/bsp/bsp_mcu_device_pn_cfg.h:
+E:/Renesas/prj/RA8P1_workspace/Embedded_Competition_Renesas_Game/ra_cfg/fsp_cfg/bsp/bsp_mcu_device_cfg.h:
+E:/Renesas/prj/RA8P1_workspace/Embedded_Competition_Renesas_Game/ra/fsp/src/bsp/mcu/ra6m5/bsp_mcu_info.h:
+E:/Renesas/prj/RA8P1_workspace/Embedded_Competition_Renesas_Game/ra/fsp/src/bsp/mcu/ra6m5/bsp_elc.h:
+E:/Renesas/prj/RA8P1_workspace/Embedded_Competition_Renesas_Game/ra/fsp/src/bsp/mcu/ra6m5/bsp_feature.h:
+E:/Renesas/prj/RA8P1_workspace/Embedded_Competition_Renesas_Game/ra/fsp/src/bsp/mcu/ra6m5/bsp_peripheral.h:
+E:/Renesas/prj/RA8P1_workspace/Embedded_Competition_Renesas_Game/ra_cfg/fsp_cfg/bsp/bsp_mcu_ofs_cfg.h:
+E:/Renesas/prj/RA8P1_workspace/Embedded_Competition_Renesas_Game/ra_cfg/fsp_cfg/bsp/board_cfg.h:
+E:/Renesas/prj/RA8P1_workspace/Embedded_Competition_Renesas_Game/ra_gen/vector_data.h:
+E:/Renesas/prj/RA8P1_workspace/Embedded_Competition_Renesas_Game/ra/fsp/src/bsp/mcu/all/bsp_exceptions.h:
+E:/Renesas/prj/RA8P1_workspace/Embedded_Competition_Renesas_Game/ra/fsp/src/bsp/cmsis/Device/RENESAS/Include/renesas.h:
+E:/Renesas/prj/RA8P1_workspace/Embedded_Competition_Renesas_Game/ra/arm/CMSIS_6/CMSIS/Core/Include/cmsis_compiler.h:
+E:/Renesas/prj/RA8P1_workspace/Embedded_Competition_Renesas_Game/ra/arm/CMSIS_6/CMSIS/Core/Include/cmsis_gcc.h:
+E:/Renesas/prj/RA8P1_workspace/Embedded_Competition_Renesas_Game/ra/fsp/src/bsp/cmsis/Device/RENESAS/Include/R7FA6M5BH.h:
+E:/Renesas/prj/RA8P1_workspace/Embedded_Competition_Renesas_Game/ra/arm/CMSIS_6/CMSIS/Core/Include/core_cm33.h:
+E:/Renesas/prj/RA8P1_workspace/Embedded_Competition_Renesas_Game/ra/arm/CMSIS_6/CMSIS/Core/Include/cmsis_version.h:
+E:/Renesas/prj/RA8P1_workspace/Embedded_Competition_Renesas_Game/ra/arm/CMSIS_6/CMSIS/Core/Include/cmsis_compiler.h:
+E:/Renesas/prj/RA8P1_workspace/Embedded_Competition_Renesas_Game/ra/arm/CMSIS_6/CMSIS/Core/Include/m-profile/armv8m_mpu.h:
+E:/Renesas/prj/RA8P1_workspace/Embedded_Competition_Renesas_Game/ra/fsp/src/bsp/cmsis/Device/RENESAS/Include/system.h:
+E:/Renesas/prj/RA8P1_workspace/Embedded_Competition_Renesas_Game/ra/fsp/src/bsp/cmsis/Device/RENESAS/Include/system.h:
+E:/Renesas/prj/RA8P1_workspace/Embedded_Competition_Renesas_Game/ra/fsp/src/bsp/mcu/all/bsp_common.h:
+E:/Renesas/prj/RA8P1_workspace/Embedded_Competition_Renesas_Game/ra/fsp/inc/api/fsp_common_api.h:
+E:/Renesas/prj/RA8P1_workspace/Embedded_Competition_Renesas_Game/ra/fsp/src/bsp/mcu/all/bsp_compiler_support.h:
+E:/Renesas/prj/RA8P1_workspace/Embedded_Competition_Renesas_Game/ra/fsp/src/bsp/mcu/all/bsp_tfu.h:
+E:/Renesas/prj/RA8P1_workspace/Embedded_Competition_Renesas_Game/ra/fsp/src/bsp/mcu/all/bsp_sdram.h:
+E:/Renesas/prj/RA8P1_workspace/Embedded_Competition_Renesas_Game/ra/fsp/src/bsp/mcu/all/bsp_mmf.h:
+E:/Renesas/prj/RA8P1_workspace/Embedded_Competition_Renesas_Game/ra/fsp/src/bsp/mcu/all/bsp_ipc.h:
+E:/Renesas/prj/RA8P1_workspace/Embedded_Competition_Renesas_Game/ra/fsp/src/bsp/mcu/all/bsp_ospi_b.h:
+bsp_linker_info.h:
+E:/Renesas/prj/RA8P1_workspace/Embedded_Competition_Renesas_Game/ra/fsp/src/bsp/mcu/all/bsp_register_protection.h:
+E:/Renesas/prj/RA8P1_workspace/Embedded_Competition_Renesas_Game/ra/fsp/src/bsp/mcu/all/bsp_irq.h:
+E:/Renesas/prj/RA8P1_workspace/Embedded_Competition_Renesas_Game/ra/fsp/src/bsp/mcu/all/bsp_io.h:
+E:/Renesas/prj/RA8P1_workspace/Embedded_Competition_Renesas_Game/ra/fsp/src/bsp/mcu/all/bsp_group_irq.h:
+E:/Renesas/prj/RA8P1_workspace/Embedded_Competition_Renesas_Game/ra/fsp/src/bsp/mcu/all/bsp_clocks.h:
+E:/Renesas/prj/RA8P1_workspace/Embedded_Competition_Renesas_Game/ra/fsp/src/bsp/mcu/all/bsp_module_stop.h:
+E:/Renesas/prj/RA8P1_workspace/Embedded_Competition_Renesas_Game/ra/fsp/src/bsp/mcu/all/bsp_security.h:
+E:/Renesas/prj/RA8P1_workspace/Embedded_Competition_Renesas_Game/ra/fsp/inc/fsp_features.h:
+E:/Renesas/prj/RA8P1_workspace/Embedded_Competition_Renesas_Game/ra/fsp/inc/api/fsp_common_api.h:
+E:/Renesas/prj/RA8P1_workspace/Embedded_Competition_Renesas_Game/ra/fsp/src/bsp/mcu/all/bsp_compiler_support.h:
+E:/Renesas/prj/RA8P1_workspace/Embedded_Competition_Renesas_Game/ra/fsp/src/bsp/mcu/all/bsp_delay.h:
+E:/Renesas/prj/RA8P1_workspace/Embedded_Competition_Renesas_Game/ra/fsp/src/bsp/mcu/all/bsp_mcu_api.h:
