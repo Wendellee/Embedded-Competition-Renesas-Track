b6ea3d7b427dbd3e24b69822e9f0ff1cc72f985f
Wendell Lee <2951404774@stu.xupt.edu.cn>
2026-06-24 18:03:47 +0800

    瑞萨杯最终代码

--- ra_gen/pin_data.c
+++ ra_gen/pin_data.c
@@ -50,9 +50,6 @@ const ioport_pin_cfg_t g_bsp_pin_cfg_data[] =
           { .pin = BSP_IO_PORT_03_PIN_12, .pin_cfg = ((uint32_t) IOPORT_CFG_DRIVE_MID
                   | (uint32_t) IOPORT_CFG_PORT_DIRECTION_OUTPUT | (uint32_t) IOPORT_CFG_PORT_OUTPUT_LOW) },
 
-          { .pin = BSP_IO_PORT_03_PIN_15, .pin_cfg = ((uint32_t) IOPORT_CFG_PERIPHERAL_PIN
-                  | (uint32_t) IOPORT_PERIPHERAL_SCI0_2_4_6_8) },
-
           { .pin = BSP_IO_PORT_04_PIN_05, .pin_cfg = ((uint32_t) IOPORT_CFG_DRIVE_HIGH
                   | (uint32_t) IOPORT_CFG_NMOS_ENABLE | (uint32_t) IOPORT_CFG_PORT_DIRECTION_OUTPUT
                   | (uint32_t) IOPORT_CFG_PORT_OUTPUT_HIGH) },
@@ -73,9 +70,6 @@ const ioport_pin_cfg_t g_bsp_pin_cfg_data[] =
                   | (uint32_t) IOPORT_PERIPHERAL_SCI0_2_4_6_8) },
 
           { .pin = BSP_IO_PORT_05_PIN_06, .pin_cfg = ((uint32_t) IOPORT_CFG_PERIPHERAL_PIN
-                  | (uint32_t) IOPORT_PERIPHERAL_SCI0_2_4_6_8) },
-
-          { .pin = BSP_IO_PORT_05_PIN_12, .pin_cfg = ((uint32_t) IOPORT_CFG_PERIPHERAL_PIN
                   | (uint32_t) IOPORT_PERIPHERAL_SCI0_2_4_6_8) },
 
           { .pin = BSP_IO_PORT_06_PIN_01, .pin_cfg = ((uint32_t) IOPORT_CFG_PERIPHERAL_PIN
